398ee5bb5d4aa903e35c3a87eda55fc149306ec1
zxx-0717 <1024988917@qq.com>
2025-04-24 17:37:42 +0800

    增加汽车坐标和尺寸参数用于测试

--- garage_navigator/src/test.cpp
+++ garage_navigator/src/test.cpp
@@ -23,28 +23,6 @@
 // };
 // int number = 6;   // 无地图纯测试环境
 
-// 一楼三个通道
-// double rects_[][4][2] = {
-//         {{4.5, -6.0}, {4.0, 0.0}, {2.0, 0.0}, {2.5, -6.0}},
-//         {{4.5, -6.0}, {4.5, -8.0}, {-6.0, -8.0}, {-6.0, -6.0}},
-//         {{2.0, 0.0}, {-5.0, 0.0}, {-5.0, -2.0}, {2.0, -2.0}}
-// };
-// int number = 3;
-
-// 车库3米通道
-double rects_[][4][2] = {
-        {{-8.7, 9.3}, {-6.5, 11.5}, {18.5, -16.0}, {16.2,-18.1}}
-};
-int number = 1;
-
-// double rects_[][4][2] = {
-//         {{-10.2, 9.5}, {-6.5, 11.5}, {18.5, -16.0}, {14.7,-19.3}}
-// };
-// int number = 1;
-
-double car_coord[2] = {3.0 , -7.0};   // car的 x,y 坐标
-double car_size[3] = {3.0, 0.6, 1.0}; // car的size
-
 bool test_cancel = false;
 
 uint8_t state_current = garage_utils_msgs::msg::State::INIT;
@@ -143,7 +121,7 @@ int main(int argc, char** argv)
         node->declare_parameter("polygons", std::vector<std::string>());
         auto param_polygons = node->get_parameter("polygons").as_string_array();
         
-        number = param_polygons.size();
+        int number = param_polygons.size();
 
         for(int i = 0; i < number; i++)
         {
@@ -159,9 +137,28 @@ int main(int argc, char** argv)
         // get other params
         node->declare_parameter<bool>("test_cancel", false);
         node->declare_parameter<bool>("test_in_garage", false);
+        node->declare_parameter<std::vector<double>>("car_coord", std::vector<double>());
+        node->declare_parameter<std::vector<double>>("car_size", std::vector<double>());
 
         node->get_parameter<bool>("test_cancel", test_cancel);
         node->get_parameter<bool>("test_in_garage", test_in_garage);
+        std::vector<double> car_size_, car_coord_;
+        node->get_parameter<std::vector<double>>("car_size", car_size_);
+        node->get_parameter<std::vector<double>>("car_coord", car_coord_);
+
+        std::stringstream ss_car_size, ss_car_coord;
+        for (size_t i = 0; i < car_size_.size(); i++)
+        {
+                ss_car_size << car_size_[i] << " ";
+        }
+        RCLCPP_INFO(node->get_logger(), "car_size: [ %s]", ss_car_size.str().c_str());
+
+
+        for (size_t i = 0; i < car_coord_.size(); i++)
+        {
+                ss_car_coord << car_coord_[i] << " ";
+        }
+        RCLCPP_INFO(node->get_logger(), "car_coord: [ %s]", ss_car_coord.str().c_str());
 
         RCLCPP_INFO(node->get_logger(), "test_cancel: %s", test_cancel ? "true" : "false");
         RCLCPP_INFO(node->get_logger(), "test_in_garage: %s", test_in_garage ? "true" : "false");
@@ -203,15 +200,11 @@ int main(int argc, char** argv)
                 for (int j = 0; j < 4; j++)
                 {
                         geometry_msgs::msg::Point p_start;
-                        // p_start.x = rects_[i][j][0];
-                        // p_start.y = rects_[i][j][1];
                         p_start.x = rects2_[i][j][0];
                         p_start.y = rects2_[i][j][1];
                         p_start.z = 0.0;
                         msg.points.push_back(p_start);
                         geometry_msgs::msg::Point p_end;
-                        // p_end.x = rects_[i][(j+1)%4][0];
-                        // p_end.y = rects_[i][(j+1)%4][1];
                         p_end.x = rects2_[i][(j+1)%4][0];
                         p_end.y = rects2_[i][(j+1)%4][1];
                         p_end.z = 0.0;
@@ -229,8 +222,8 @@ int main(int argc, char** argv)
 
         geometry_msgs::msg::PoseStamped car_pose;
         car_pose.header.frame_id = "map";
-        car_pose.pose.position.x = car_coord[0];
-        car_pose.pose.position.y = car_coord[1];
+        car_pose.pose.position.x = car_coord_[0];
+        car_pose.pose.position.y = car_coord_[1];
         
         std::vector<geometry_msgs::msg::Polygon> polygons;
         garage_utils_msgs::msg::Polygons polygons_msg;
@@ -240,8 +233,6 @@ int main(int argc, char** argv)
                 for (int j = 0; j < 4; j++)
                 {
                         geometry_msgs::msg::Point32 point;
-                        // point.x = rects_[i][j][0];
-                        // point.y = rects_[i][j][1];
                         point.x = rects2_[i][j][0];
                         point.y = rects2_[i][j][1];
                         polygon.points.push_back(point);
@@ -268,9 +259,9 @@ int main(int argc, char** argv)
         auto goal_msg = garage_utils_msgs::action::GarageVehicleAvoidance::Goal();
         capella_ros_msg::msg::CarDetectSingle car_information;
         geometry_msgs::msg::Vector3 size;
-        size.x = car_size[0];
-        size.y = car_size[1];
-        size.z = car_size[2];
+        size.x = car_size_[0];
+        size.y = car_size_[1];
+        size.z = car_size_[2];
         car_information.pose = car_pose;
         car_information.size = size;
 
